50978a68c37d21da66d47adc0ad21bf8be6e4c81
Aiswarya Krishnan <aswathikrishnan204@gmail.com>
2020-08-13 17:37:08 +0530

    done

---
+++ form/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import{ReactiveFormsModule} from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ form/src/app/form/form.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import{User} from './../users';
+import{FormBuilder,FormGroup,FormControl} from '@angular/forms';
+import{Validators} from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  registerForm :FormGroup;
+  submitted  = false;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required, Validators.minLength(3)],
+      lastName: ['', Validators.required, Validators.minLength(1)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  get form() { return this.registerForm.controls; }
+
+  onSubmit() {
+    // console.log(this.userform.value);
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 3));
+    console.log(JSON.stringify(this.registerForm.value));
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
+}
